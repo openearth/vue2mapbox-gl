18ba08d0de089cbdf1d9582231bd06fd820c4ec7
Fedor Baart <fedor.baart@deltares.nl>
2017-12-17 16:10:11 +0100

    expose other methods

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import VMapbox from './components/VMapbox.vue';
+import VMapboxLayer from './components/VMapboxLayer.vue';
+import VMapboxSource from './components/VMapboxSource.vue';
+import VMapboxNavigationControl from './components/VMapboxNavigationControl.vue';
 
 function install(Vue, options) {
   console.log('Installing', Vue, options);
   Vue.component('v-mapbox', VMapbox);
+  Vue.component('v-mapbox-layer', VMapboxLayer);
+  Vue.component('v-mapbox-source', VMapboxSource);
+  Vue.component('v-mapbox-navigation-control', VMapboxNavigationControl);
 };
 export default install;
