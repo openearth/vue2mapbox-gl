088392f1310998fbb1300fb6ac9a46c8310c83ae
Fedor Baart <fedor.baart@deltares.nl>
2019-06-21 16:42:45 +0200

    update tests although not used

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 import test from 'ava';
-import fn from '.';
+import install from '.';
+import Vue from 'vue'
+
 
 test('install', t => {
-	t.is(fn(), 'unicorns & rainbows');
+  Vue.use(install)
+  // get the component
+  // Testing is not working in node, consider testing in a browser component
+  // https://github.com/mapbox/mapbox-gl-js/issues/4593
+  let VMapbox = Vue.component('v-mapbox')
+	t.truthy(VMapbox);
 });
