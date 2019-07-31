4329d30ed305c59c2b050044987926ca84426d27
Fedor Baart <fedor.baart@deltares.nl>
2019-07-31 15:01:21 +0200

    create test for add/remove components

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -53,6 +53,15 @@ const styleTemplate = `
 </v-mapbox>
 `
 
+const injectTemplate = `
+<v-mapbox
+ map-style="mapbox://styles/mapbox/satellite-streets-v10"
+ access-token="pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
+ style="height: 300px;"
+>
+ <v-mapbox-geolocate-control v-if="geoLocateControl"></v-mapbox-geolocate-control>
+</v-mapbox>
+`
 
 
 
@@ -131,6 +140,22 @@ storiesOf('Map', module)
         light () {
           this.style = 'mapbox://styles/global-data-viewer/cjtslsula05as1fppvrh7n4rv'
         }
+      }
+    }
+  })
+  .addDecorator(withKnobs)
+  .add('add/remove control', () => {
+    return {
+      template: injectTemplate,
+      data () {
+        return {
+          geoLocateControl: true
+        }
+      },
+      mounted () {
+        button('toggle control', () => {
+          this.geoLocateControl = !this.geoLocateControl
+        })
       }
     }
   })
